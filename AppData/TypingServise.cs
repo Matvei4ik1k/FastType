e30ad92eb58c708c85796a9d64a17f4eb097cdda
Дьячков.Матвей <.@TERM1>
2025-03-17 11:09:36 +0300

    Дали тег буквам

--- AppData/TypingServise.cs
+++ AppData/TypingServise.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Controls;
 
 namespace FastType.AppData
 {
-    internal class TypingServise
+    public class TypingServise
     {
+        //Поля для хранения и использования в рамках класса.
+        private Grid _keyboardGrid;
+        private TextBox _typingTextBox;
+        private TextBlock _typintTextBlock;
+
+        //Создаем конструктор класса для приема элементов управления из интерфейса (TypingTutorPage.xaml)
+        public TypingServise(Grid keyboardGrid, TextBox typingTextBox, TextBlock typintTextBlock)
+        {
+            _keyboardGrid = keyboardGrid;
+            _typingTextBox = typingTextBox;
+            _typintTextBlock = typintTextBlock;
+            _typingTextBox.PreviewKeyDown += _typingTextBox_PreviewKeyDown;
+            _typingTextBox.PreviewKeyUp += _typingTextBox_PreviewKeyUp;
+        }
+
+        private void _typingTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+        }
+
+        private void _typingTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            _typingTextBox.Clear();
+            _typingTextBox.Text = e.Key.ToString();
+        }
     }
 }
