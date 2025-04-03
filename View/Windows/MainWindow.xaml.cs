191022376fa33d0e2a9e76cb513ce2a29cea0ce6
Дьячков.Матвей <.@TERM1>
2025-04-03 16:27:02 +0300

    ...

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace FastType
 {
@@ -26,6 +13,16 @@ namespace FastType
         }
 
         private void TypingTutorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFraime.Navigate();
+        }
+
+        private void RatingBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProfileBtn_Click(object sender, RoutedEventArgs e)
         {
 
         }
